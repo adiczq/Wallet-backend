e9bbc454fa86e5442335cc239a222602a6f3c030
Adrian Lacheta <adiczq@gmail.com>
2024-02-15 16:58:35 +0100

    Update sendEmailHelper.js

--- utils/sendEmailHelper.js
+++ utils/sendEmailHelper.js
@@ -2,11 +2,19 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = (url, email) => {
+  // const msg = {
+  //   to: email,
+  //   from: 'adiczq@gmail.com',
+  //   subject: 'Email verification',
+  //   text: `Verify your email by clicking on the link - ${url}`,
+  // };
   const msg = {
     to: email,
     from: 'adiczq@gmail.com',
-    subject: 'Email verification',
-    text: `Verify your email by clicking on the link - ${url}`,
+    dynamicTemplateData: {
+      url: url,
+    },
+    templateId: 'd-e4c7c066815c437388885b7c964d5cfc',
   };
   sgMail
     .send(msg)
