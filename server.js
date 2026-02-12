90c5995388e89c55ebb512bf39191b3fa1e89ae9
Adrian Lacheta <adiczq@gmail.com>
2026-02-12 12:08:37 +0100

    listen

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
 
 // Server launching
 const PORT = process.env.PORT || 3002;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port: ${PORT}`);
 });
 
